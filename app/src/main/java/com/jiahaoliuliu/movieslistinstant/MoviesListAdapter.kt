434202ed5727218fb5c4704c7f3881324e2beabb
Jiahao Liu <jiahao.liu@dnata.com>
2020-04-01 14:07:55 +0400

    Set up the movies list

--- app/src/main/java/com/jiahaoliuliu/movieslistinstant/MoviesListAdapter.kt
+++ app/src/main/java/com/jiahaoliuliu/movieslistinstant/MoviesListAdapter.kt
@@ -1,21 +1,27 @@
 package com.jiahaoliuliu.movieslistinstant
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.jiahaoliuliu.movieslistinstant.databinding.ItemMovieBinding
 
-class MoviesListAdapter(val moviesList: List<Movie>): RecyclerView.Adapter<MovieViewHolder>() {
+class MoviesListAdapter(private val moviesList: List<Movie>): RecyclerView.Adapter<MovieViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        TODO("Not yet implemented")
+        return MovieViewHolder(ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun getItemCount() = moviesList.size
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(moviesList[position])
     }
 }
 
-class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class MovieViewHolder(private val movieItemBinding: ItemMovieBinding) : RecyclerView.ViewHolder(movieItemBinding.root) {
 
+    fun bind(movie: Movie) {
+        movieItemBinding.movie = movie
+        movieItemBinding.executePendingBindings()
+    }
 }
