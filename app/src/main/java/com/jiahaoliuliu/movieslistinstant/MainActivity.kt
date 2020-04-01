aff85f0609612f0f1ad85a0e916d4c2c32a36037
Jiahao Liu <jiahao.liu@dnata.com>
2020-04-01 15:47:52 +0400

    Set up the click listener

--- app/src/main/java/com/jiahaoliuliu/movieslistinstant/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/movieslistinstant/MainActivity.kt
@@ -2,16 +2,24 @@ package com.jiahaoliuliu.movieslistinstant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMovieClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.movies_list)
         val moviesRepository = MoviesRepository()
-        val moviesListAdapter = MoviesListAdapter(moviesRepository.getMoviesList())
+        val moviesListAdapter = MoviesListAdapter(moviesRepository.getMoviesList(), this)
         recyclerView.adapter = moviesListAdapter
+    }
+
+    override fun onMovieClicked(id: Int) {
+        // Open movie details
+        Toast.makeText(this, "Click on the item $id", Toast.LENGTH_LONG).show()
     }
 }
